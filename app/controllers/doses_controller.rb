7f8d92237b1f57982e1530bbc05734071237e6d7
Tess Saffar <tess.saf@hotmail.fr>
2017-11-23 14:48:53 +0000

    ex1 suite

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,25 @@
 class DosesController < ApplicationController
+ def index
+    @doses = Dose.all       # GET /restaurants
+  end
+
+ def new
+    @dose = dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+     if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, )
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon"),
+Ingredient.create(name: "ice"),
+Ingredient.create(name: "mint leaves"),
+
+Cocktail.create(name: "Mojito"),
+Cocktail.create(name: "Spritz"),
+Cocktail.create(name: "Mai tai"),
+
+Dose.create(description: “6cl”, ingredient_id: 1, cocktail_id: 1)
+Dose.create(description: “10cl”, ingredient_id: 2, cocktail_id: 1)
+Dose.create(description: “5cl”, ingredient_id: 3, cocktail_id: 1)
